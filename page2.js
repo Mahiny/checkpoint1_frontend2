24cf0cc0d1eb4f6dd541eeb004a13e3983818326
ErikSD2 <you@example.com>
2022-06-08 00:33:51 -0300

    image

--- page2.js
+++ page2.js
@@ -2,3 +2,8 @@ const {imageUrl, title, description} = JSON.parse(localStorage.getItem('card'))
 const photo = document.querySelector('.photo')
 const titleText = document.querySelector('.title')
 const descriptionText = document.querySelector('.description')
+
+
+photo.src=imageUrl
+titleText.innerHTML=title
+descriptionText.innerHTML=description
