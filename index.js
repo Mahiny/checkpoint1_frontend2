ccd589725b6a84ff8445d9b4b894c27a0f60c96f
ErikSD2 <you@example.com>
2022-06-08 00:55:25 -0300

    format

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const form = document.querySelector("form")
-form.addEventListener("submit",()=>{
-    const dados = Object.fromEntries(new FormData(form).entries())
-    localStorage.setItem('card', JSON.stringify(dados));
-})
+const form = document.querySelector("form");
+form.addEventListener("submit", () => {
+  const dados = Object.fromEntries(new FormData(form).entries());
+  localStorage.setItem("card", JSON.stringify(dados));
+});
--- page2.js
+++ page2.js
@@ -1,9 +1,10 @@
-const {imageUrl, title, description} = JSON.parse(localStorage.getItem('card'))
-const photo = document.querySelector('.photo')
-const titleText = document.querySelector('.title')
-const descriptionText = document.querySelector('.description')
+const { imageUrl, title, description } = JSON.parse(
+  localStorage.getItem("card")
+);
+const photo = document.querySelector(".photo");
+const titleText = document.querySelector(".title");
+const descriptionText = document.querySelector(".description");
 
-
-photo.src=imageUrl
-titleText.innerHTML=title
-descriptionText.innerHTML=description
+photo.src = imageUrl;
+titleText.innerHTML = title;
+descriptionText.innerHTML = description;
