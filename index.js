c56f68ffcd3e3ce652844959ff676fc2b322d5a1
ErikSD2 <you@example.com>
2022-06-13 23:38:59 -0300

    2 cards

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+let cards = [];
+
 const form = document.querySelector("form");
-form.addEventListener("submit", () => {
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
   const dados = Object.fromEntries(new FormData(form).entries());
-  localStorage.setItem("card", JSON.stringify(dados));
+  if (cards.length == 1) {
+    cards.push(dados);
+    window.location.href = "/page2.html";
+    localStorage.setItem("card", JSON.stringify(cards));
+    return
+  }
+  cards.push(dados);
+  form.reset();
 });
--- page2.js
+++ page2.js
@@ -1,10 +1,26 @@
-const { imageUrl, title, description } = JSON.parse(
-  localStorage.getItem("card")
-);
-const photo = document.querySelector(".photo");
+const cards = JSON.parse(localStorage.getItem("card"));
+cards.map((card)=>createcard(card))
+console.log(card)
+
+function createcard({ imageUrl, title, description }) {
+  const grade = document.querySelector('.grade')
+  const div = document.createElement("div");
+  div.setAttribute("class", "centro");
+
+  div.innerHTML = `<div class="card2">
+    <img src="${imageUrl}" class="photo" />
+    <div class="title">${title}</div>
+    <div class="description">${description}</div>
+  </div>`;
+
+  grade.appendChild(div)
+}
+/* const photo = document.querySelector(".photo");
 const titleText = document.querySelector(".title");
 const descriptionText = document.querySelector(".description");
 
 photo.src = imageUrl;
 titleText.innerHTML = title;
-descriptionText.innerHTML = description;
+descriptionText.innerHTML = description; */
+
+
